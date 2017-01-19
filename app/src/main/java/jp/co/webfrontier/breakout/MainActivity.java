9ac54071666146fed2779a1ca92fd7499ca3c58a
Kazuyuki Someya <k_someya@webfrontier.co.jp>
2017-01-19 19:57:46 +0900

    importもれ

--- app/src/main/java/jp/co/webfrontier/breakout/MainActivity.java
+++ app/src/main/java/jp/co/webfrontier/breakout/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Button;
 
 import org.json.JSONObject;
 
