753e13ec4fc183e4145c4bd55ea6480009db92bc
Kazuyuki Someya <k_someya@webfrontier.co.jp>
2017-02-04 01:38:24 +0900

    課題番号の間違いを修正

--- app/src/main/java/jp/co/webfrontier/breakout/BrickNormal.java
+++ app/src/main/java/jp/co/webfrontier/breakout/BrickNormal.java
@@ -173,7 +173,7 @@ public class BrickNormal extends Brick {
         final int r = robustness.getValue();
         if(r == hitCount) {
             broken = true;
-            /** A-05. 効果音を鳴らす
+            /** A-04. 効果音を鳴らす
              * ゲームの効果音をつける
              * ブロックを破壊したとき, 破壊できないブロックに当たったとき,パッドで反射したとき
              * ボールをロストしたとき, ゲームをクリアしたとき, ゲームオーバーになったとき
