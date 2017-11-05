903b6945a360bdf37874ba6d61b3b7bfd5dc650d
s_yano <s_yano@webfrontier.co.jp>
2017-11-05 17:13:29 +0900

    B-4削除

--- app/src/main/java/jp/co/webfrontier/breakout/Breakout.java
+++ app/src/main/java/jp/co/webfrontier/breakout/Breakout.java
@@ -177,12 +177,6 @@ public class Breakout {
         initializeBall();
         addBall(fieldRect.width()/2, fieldRect.height()/2);
 
-        /**
-         * B-04．ブロックを表示させる
-         * ブロックを生成し初期位置に配置する
-         */
-        createBrick();
-        initializeBrick();
     }
 
     /**
